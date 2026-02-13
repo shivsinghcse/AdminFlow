15283ff244fe97480ae689bdfb8e249e9884f0ff
shivsinghcse <shivsinghcse@gmail.com>
2026-02-13 10:20:32 +0530

    feat: design classes page

---
+++ script/classes.js
@@ -0,0 +1,13 @@
+
+
+const createClass = (e) => {
+    e.preventDefault()
+    const classes = document.getElementById('class').value.trim()
+    const sections = document.getElementById('sections').value.trim()
+    const classTeacher = document.getElementById('classTeacher').value.trim()
+    const fee = document.getElementById('fee').value.trim()
+
+    console.log(classes, sections, classTeacher, fee);
+
+    e.target.reset()
+}
