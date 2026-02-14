af299f28316a8699ee39aeafc74a96ca0ec06026
shivsinghcse <shivsinghcse@gmail.com>
2026-02-15 04:31:59 +0530

    feat: Design expense page

---
+++ script/expenses.js
@@ -0,0 +1,13 @@
+
+const createExpenses = (e) => {
+    e.preventDefault()
+    
+    const title = document.getElementById('title').value.trim();
+    const amount = document.getElementById('amount').value.trim();
+    const date = document.getElementById('date').value.trim();
+    const description = document.getElementById('description').value.trim();
+
+    console.log(title, amount, date, description);
+
+    e.target.reset()
+}
---
+++ script/salaries.js
@@ -0,0 +1,13 @@
+const createSalary = (e) => {
+    e.preventDefault()
+    
+    const employee = document.getElementById('employee').value.trim()
+    const salary = document.getElementById('salary').value.trim()
+    const date = document.getElementById('date').value.trim()
+    const description = document.getElementById('description').value.trim()
+
+    console.log(employee, salary, date, description);
+
+    e.target.reset()
+
+}
