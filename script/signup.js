6deef944b57b93a53e142c6e7ed21317c0a36ce8
shivsinghcse <shivsinghcse@gmail.com>
2026-02-16 02:15:58 +0530

    feat: connection signup page to backend

--- script/signup.js
+++ script/signup.js
@@ -1,10 +1,39 @@
 const signup = (e) => {
     e.preventDefault();
     const form = e.target
-    const fullname = form.elements.fullname.value.trim()
-    const mobileNumber = form.elements.mobileNumber.value.trim()
+    const schoolName = form.elements.schoolName.value.trim()
+    const dirName = form.elements.dirName.value.trim()
+    const mobile = form.elements.mobileNumber.value.trim()
     const email = form.elements.email.value.trim()
     const password = form.elements.password.value.trim()
 
-    console.log(fullname, mobileNumber, email, password);
+    const payload = {
+        schoolName,
+        dirName, 
+        mobile,
+        email,
+        password
+    }
+
+    form.reset()
+
+    fetch("http://localhost:8080/school/signup", {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(payload)
+    })
+    
+    .then((res) => {
+        return res.json()
+    })
+    
+    .then((data) => {
+        console.log(data);
+    })
+    
+    .catch((err) => {
+        console.log(err.message);
+    })
 }
